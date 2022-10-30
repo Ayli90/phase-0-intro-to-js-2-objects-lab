94602191f5901e7424adc6edc5828d8d76d94f2f
Amy Li <amyl@buffalo.edu>
2022-10-29 22:43:57 -0400

    commit tree ce0ecaefc06e5449c9d37b05a7777234958f8e11

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+//Make a object name employee with two keys.
+const employee = {name: 'Sam',
+    key: '11 Broadway'} 
+
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    // copy object with ...
+    const newemployee = {...employee};
+    // Add key and value to employee using dot notation
+    newemployee[key] = value
+    //return employee
+    return (newemployee)
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newemployee = {...employee}
+    delete newemployee[key] 
+    return (newemployee)
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return (employee)
+}
